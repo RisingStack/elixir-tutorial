e000e82d6ba47443b925c0bd16aba1b11dd1baa1
Shadowbeetle <tamas.kadlecsik@gmail.com>
2020-12-12 21:27:24 +0100

    add yield

---
+++ 2_task/5_async_yield.exs
@@ -0,0 +1,26 @@
+create_task = fn ->
+  Task.async(fn ->
+    Process.sleep(100)
+    "I'm slow and async"
+  end)
+end
+
+run_task = fn task, timeout ->
+  case Task.yield(task, timeout) || Task.shutdown(task) do
+    {:ok, result} ->
+      IO.puts(result)
+
+    nil ->
+      IO.puts(IO.puts("Failed to get a result in #{timeout}ms"))
+
+      nil
+  end
+end
+
+task1 = create_task.()
+IO.puts("I'm doing some other stuff")
+run_task.(task1, 500)
+
+task2 = create_task.()
+IO.puts("I'm doing some other stuff")
+run_task.(task2, 10)
