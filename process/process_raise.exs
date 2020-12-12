5d051d93c4e226f012a1d1a84f02ac6ab2473501
Shadowbeetle <tamas.kadlecsik@gmail.com>
2020-12-12 20:25:58 +0100

    process raise

---
+++ process/process_raise.exs
@@ -0,0 +1,27 @@
+defmodule ProcessTutorial do
+  def start_sender_process(pid, {type, message}) do
+    spawn(fn -> send(pid, {type, message}) end)
+  end
+
+  defp receive_message do
+    receive do
+      {:hello, message} -> IO.puts("Hello, world! #{message}")
+      {:error, reason} -> raise reason
+      {_, message} -> IO.puts(message)
+    end
+
+    receive_message()
+  end
+
+  def start_receiver_process do
+    spawn(&receive_message/0)
+  end
+end
+
+pid = ProcessTutorial.start_receiver_process()
+
+ProcessTutorial.start_sender_process(pid, {:error, "Oops!"})
+
+ProcessTutorial.start_sender_process(pid, {:ok, "Started"})
+
+ProcessTutorial.start_sender_process(pid, {:hello, "Look what I can do!"})
