202de630b6d90bb49988c03f4dc6485ac57248ee
Shadowbeetle <tamas.kadlecsik@gmail.com>
2021-01-10 18:29:20 +0100

    add crash test

--- 1_process/3_raise.exs
+++ 1_process/3_raise.exs
@@ -5,3 +5,7 @@ IO.puts("I'm fine")
 spawn_link(fn -> raise "oops" end)
 
 IO.puts("I'll die now")
+
+Process.sleep(1000)
+
+IO.puts("I should not be printed")
---
+++ crash_test.exs
@@ -0,0 +1,43 @@
+defmodule CrashTest.GenServer do
+  use GenServer
+
+  @impl true
+  def init(_) do
+    {:ok, :alive}
+  end
+
+  @impl true
+  def handle_call({:crash, :sync}, _from, state) do
+    raise "oops"
+
+    IO.puts("I'm synchronously dead")
+
+    {:reply, state, state}
+  end
+  @impl true
+  def handle_call({:crash, :spawn}, _from, state) do
+    spawn_link(fn -> raise "oops" end)
+
+    IO.puts("I'm killed by my child process")
+
+    {:reply, state, state}
+  end
+
+  @impl true
+  def handle_call({:crash, :task}, _from, state) do
+    Task.start_link(fn -> raise "oops" end)
+
+    IO.puts("I'm fine")
+
+    Process.sleep(1000)
+
+    IO.puts("I should not be printed")
+
+    {:reply, state, state}
+  end
+end
+
+{:ok, server} = GenServer.start_link(CrashTest.GenServer, nil)
+GenServer.call(server, {:crash, :task}) |> inspect |> IO.puts()
+GenServer.call(server, {:crash, :spawn}) |> inspect |> IO.puts()
+GenServer.call(server, {:crash, :sync}) |> inspect |> IO.puts()
